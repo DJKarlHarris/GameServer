bf32a83ab2ee88960e4d92b8afb9624eb05b530c
djkarlharris <1149338863@qq.com>
2023-07-06 21:41:41 +0800

    add build

---
+++ GameClient/build/bin/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/workspace/GameServer/GameClient/src/main.cpp" "bin/CMakeFiles/main.dir/main.cpp.o" "gcc" "bin/CMakeFiles/main.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/workspace/GameServer/GameClient/build/bin/CMakeFiles/protolib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
